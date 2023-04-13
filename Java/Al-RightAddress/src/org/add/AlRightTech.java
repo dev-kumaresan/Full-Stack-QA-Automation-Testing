e46e6d1a29dcd0811f79e60ac10024143635ce80
dev-kumaresan <dev.kumaresanl@gmail.com>
2023-04-13 14:55:02 +0530

    Al-right

---
+++ Java/Al-RightAddress/src/org/add/AlRightTech.java
@@ -0,0 +1,34 @@
+package org.add;
+
+public class AlRightTech {
+
+	public void Al_RightOmr() {
+		System.out.println("Tech in omr");
+	}
+
+	public void Al_RightAdayar() {
+		System.out.println("Tech in Adayar");
+	}
+
+	public void Al_RightTambaram() {
+		System.out.println("Tech in Tambaram");
+	}
+
+	public void Al_RightVelacherry() {
+		System.out.println("Tech in Velacherry");
+	}
+
+	public void Al_RightAnnaNagar() {
+		System.out.println("Tech in Anna Nagar");
+	}
+
+	public static void main(String[] args) {
+		AlRightTech AR = new AlRightTech();
+		AR.Al_RightOmr();
+		AR.Al_RightAdayar();
+		AR.Al_RightTambaram();
+		AR.Al_RightVelacherry();
+		AR.Al_RightAnnaNagar();
+	}
+
+}
