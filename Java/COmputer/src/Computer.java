8ae54ed3e8f3d67c6809ca8cb129cf42ae0ce38b
dev-kumaresan <dev.kumaresanl@gmail.com>
2023-04-17 11:22:57 +0530

    Commit

---
+++ Java/COmputer/src/Computer.java
@@ -0,0 +1,8 @@
+
+public class Computer {
+
+	public void computerModel() {
+		System.out.println("Computer Model : DELL 1678");
+	}
+
+}
---
+++ Java/COmputer/src/Desktop.java
@@ -0,0 +1,15 @@
+
+public class Desktop extends Computer {
+
+	public void desktopSize() {
+		System.out.println("Desktop Size : 11x33 inches");
+	}
+
+	public static void main(String[] args) {
+		Desktop D = new Desktop();
+		D.computerModel();
+		D.desktopSize();
+
+	}
+
+}
