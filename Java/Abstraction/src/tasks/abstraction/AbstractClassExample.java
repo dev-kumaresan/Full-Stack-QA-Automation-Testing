cb9073e071f85052093fb2dd0d4205f7c9ae314d
dev-kumaresan <dev.kumaresanl@gmail.com>
2023-04-22 11:19:24 +0530

    commit

--- Java/Abstraction/src/tasks/abstraction/AbstractClassExample.java
+++ Java/Abstraction/src/tasks/abstraction/AbstractClassExample.java
@@ -1,10 +1,14 @@
 package tasks.abstraction;
 
 public abstract class AbstractClassExample {
+	final int a = 5; // final variable
+	static int b=10; // static variable;
+	int c,d;
+	public abstract void studentMarkDetails(); // ABSTRACT METHOD
 
-	public abstract void studentMarkDetails(); // Partially abstraction
-
-	public void studentDetails() {
+	public void studentDetails() { // NON-ABSTRACT METHOD
 		System.out.println("Student Name : Kumaresan L");
+		System.out.println(a);
+		System.out.println(b);
 	}
 }
--- Java/Abstraction/src/tasks/abstraction/StudentMarks.java
+++ Java/Abstraction/src/tasks/abstraction/StudentMarks.java
@@ -3,6 +3,8 @@ package tasks.abstraction;
 public class StudentMarks extends AbstractClassExample {
 	public void studentMarkDetails() {
 		System.out.println("Total : 394/500 in 10th class");
+		System.out.println(AbstractClassExample.b = 11); // inherit static variable and print
+		System.out.println(a + 10); // inherit final variable and modify the value
 	}
 
 	public void studentAddress() {
