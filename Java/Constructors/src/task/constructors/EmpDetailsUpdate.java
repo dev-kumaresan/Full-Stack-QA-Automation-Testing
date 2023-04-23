fa66793ccb4f38ec9f937328603d075b50583736
dev-kumaresan <dev.kumaresanl@gmail.com>
2023-04-23 10:10:33 +0530

    commit

---
+++ Java/Constructors/src/task/constructors/EmpDetailsUpdate.java
@@ -0,0 +1,14 @@
+package task.constructors;
+
+public class EmpDetailsUpdate extends EmpDetails {
+
+	EmpDetailsUpdate() {
+		super(80.9);
+	}
+
+	public static void main(String[] args) {
+
+		EmpDetailsUpdate EDU = new EmpDetailsUpdate();
+	}
+
+}
