c770bcec6f04cd5ceaf127203dba3a5a39094b1c
dev-kumaresan <dev.kumaresanl@gmail.com>
2023-04-29 08:23:35 +0530

    commit

---
+++ Java/Tests/src/Emp.java
@@ -0,0 +1,25 @@
+
+public class Emp {
+	int a;
+	static int b;
+
+	public void fun1() {
+		System.out.println("This is fun1");
+	}
+
+	public static void fun2() {
+		System.out.println("This is fun2");
+	}
+
+	public static void main(String[] args) {
+		Emp E = new Emp();
+		E.a = 10;
+		System.out.println(E.a);
+		b = 20;
+		System.out.println(b);
+		E.fun1();
+		fun2();
+
+	}
+
+}
