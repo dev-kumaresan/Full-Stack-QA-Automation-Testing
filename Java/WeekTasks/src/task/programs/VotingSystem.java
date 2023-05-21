a6be361806bebf6f31411ddbb38172025a5fadff
Kumaresan L <dev.kumaresanl@gmail.com>
2023-05-21 09:35:47 +0530

    commit

--- Java/WeekTasks/src/task/programs/VotingSystem.java
+++ Java/WeekTasks/src/task/programs/VotingSystem.java
@@ -3,6 +3,7 @@ package task.programs;
 import java.util.Scanner;
 
 public class VotingSystem {
+	
 
 	public static void main(String[] args) {
 		Scanner getage = new Scanner(System.in);
@@ -10,8 +11,11 @@ public class VotingSystem {
 		int voters_age = getage.nextInt();
 		if (voters_age >= 18) {
 			System.out.println("You are eligible to vote *");
+		    
+			
 		} else {
 			System.out.println("You are not eligible to vote");
 		}
+		
 	}
 }
