fb5b64afc28363dc05b3ef0156d6868eb36f1a7c
Kumaresan L <dev.kumaresanl@gmail.com>
2023-09-13 08:14:15 +0530

    Api-testing

---
+++ API Testing/APITesting/src/test/java/mydata/MyData.java
@@ -0,0 +1,29 @@
+package mydata;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class MyData {
+
+	public static void main(String[] args) throws IOException, ParseException {
+		
+		FileReader fr = new FileReader("/home/kumaresan/eclipse-workspace/APITesting/src/test/resources/json/sample.json");
+		JSONParser jp = new JSONParser();
+		Object parse = jp.parse(fr);
+		JSONObject job = (JSONObject)parse;
+		String name = (String) job.get("Name");
+		System.out.println(name);
+		JSONArray ja = (JSONArray) job.get("SelfProjects");
+		for(Object a : ja) {
+			System.out.println(a);
+		}
+		
+
+	}
+
+}
